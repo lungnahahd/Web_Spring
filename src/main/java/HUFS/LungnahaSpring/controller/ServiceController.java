b7c1ceaec1e1e8abbf568259b2261ae223063d16
lungnahahd <jyoungjk33@gmail.com>
2021-01-22 11:35:08 +0900

    Add_list(회원 조회 기능 구현)

--- src/main/java/HUFS/LungnahaSpring/controller/ServiceController.java
+++ src/main/java/HUFS/LungnahaSpring/controller/ServiceController.java
@@ -3,9 +3,14 @@ package HUFS.LungnahaSpring.controller;
 import HUFS.LungnahaSpring.clientinfo.Member;
 import HUFS.LungnahaSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+@Controller
 public class ServiceController {
     private final MemberService memberService;
 
@@ -14,16 +19,23 @@ public class ServiceController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/members/new")
+    @GetMapping(value = "/members/new")
     public String CreatedMember(){
-        return "createmember";
+        return "create";
     }
 
-    @PostMapping("/members/new")
-    public String Create(MemberForm form){
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findAllMember();
+        model.addAttribute("members",members);
+        return "memberList";
     }
 }
